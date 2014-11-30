7db39fe7cb441f5157f794d86362f3bbf7b3f65e
Christ Schlacta <aarcane@aarcane.org>
2014-11-29 21:51:24 -0800

    Converted all backgrounds to SpriteRenderer prefabs.
    added a prefab for the dynamic background in InfiniteMode
    added InfiniteMode
    InfiniteMode is NOT playable yet.
    Modifies ALL .unity levels.

---
+++ FlappyLand!/Assets/BackgroundChangable.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundChangable : MonoBehaviour
+{
+	/***
+	 * This array contains the available backgrounds, one of which is
+	 * chosen randomly.
+	 */
+	public SpriteRenderer[] backgrounds;
+	int prev;
+	int current;
+	bool fadeIn = false;
+	bool fadeOut = false;
+	public float fadeTime;
+	public int CycleTime;
+	int currentCycleTime;
+	float fadeStart = 0f;
+	// Use this for initialization
+	void Start ()
+	{	for (int i = 0; i < backgrounds.Length; ++i)
+		{	backgrounds[i] = Instantiate(backgrounds[i]) as SpriteRenderer;
+			backgrounds[i].color = new Color(1f, 1f, 1f, 0f);
+		}
+		Time.timeScale = 1f;
+		current = prev = 0;
+		SelectNextBackground ();
+		fadeOut = false;
+		fadeIn = false;
+		backgrounds [current].color = new Color (1f, 1f, 1f, 1f);
+		if(CycleTime > 0)
+			InvokeRepeating ("SwitchBackground", 1f, 1f);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{	float Fade = Mathf.SmoothStep(0f, 1f, ((Time.time - fadeStart) / fadeTime));
+		if (fadeIn)
+			backgrounds[current].color = new Color(1f, 1f, 1f, Fade);
+
+		if (fadeOut)
+			backgrounds[prev].color = new Color(1f, 1f, 1f, (1-Fade));
+
+		if (Fade >= 1f)
+		{	fadeIn = false;
+			fadeOut = false;
+			backgrounds[current].color = new Color(1f, 1f, 1f, 1f);
+			backgrounds[prev].color = new Color(1f, 1f, 1f, 0f);
+	}	}
+	void SelectNextBackground ()
+	{	prev = current;
+		Debug.Log ("SelectNextBackground()");
+		while (prev == current)
+			current = Random.Range (0, backgrounds.Length);
+		currentCycleTime = CycleTime;
+		fadeStart = Time.time;
+		fadeIn = true;
+		fadeOut = true;
+	}
+	void SwitchBackground()
+	{	Debug.Log ("Random.Range(0, " + currentCycleTime);
+		if (Random.Range (0, currentCycleTime) == 0)
+			SelectNextBackground();
+		else
+			--currentCycleTime;
+}	}
