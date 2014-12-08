c5ddff79fccb06a6995052d854cac055b58168e0
eguan1 <eguan1@uci.edu>
2014-12-07 23:44:17 -0800

    Fixed Item Properties
    Fixed size of money item collider, fixed pipes, now generate random X
    and Y positions

--- FlappyLand!/Assets/Scripts/PipeObstacle.cs
+++ FlappyLand!/Assets/Scripts/PipeObstacle.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class PipeObstacle : MonoBehaviour {
 
 	public Vector2 velocity = new Vector2(-4, 0);
-	public float rangeY = 5f;
-	
+	private float rangeY = 1.8f;
+	private float rangeX = 1.3f;
 	// Use this for initialization
 	void Start()
 	{
 		rigidbody2D.velocity = velocity * Random.value;
-		//transform.position = new Vector3(transform.position.x, transform.position.y - rangeY * Random.value, transform.position.z);
-		//Destroy(gameObject, 80);
+		transform.position = new Vector3(transform.position.x + rangeX * Random.value , transform.position.y - rangeY * Random.value, transform.position.z);
+		Destroy(gameObject, 80);
 	}
 
 
